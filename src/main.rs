e53e94d966d02c5baa4eabe5109bfa390edf028d
Claire Moore <nrmoore@uci.edu>
2018-12-24 13:44:06 -0800

    Add parsing and basic output
    Will be able to parse and get the text.
    Only supports Artist, Title, and Album, not label nor genre

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate roxmltree;
 
 use std::fmt;
+use std::env;
+use std::fs;
+use std::io::Read;
+use std::process;
 
 struct Song {
     title: String,
@@ -15,9 +19,41 @@ impl fmt::Display for Song {
 }
 
 fn main() {
-    let songs: Vec<Song> = vec![Song{
-        title: String::from("風の日"),
-        artist: String::from("ELLEGARDEN"),
-        album: String::from("Figureheads Compilation")}];
-    print!("{}", songs[0]);
+    let args: Vec<_> = env::args().collect();
+    let songs: Vec<Song> = get_songs(&args[1]);
+    for song in songs {
+        println!("{}", song);
+    }
+}
+
+fn get_songs(xml_path: &str) -> Vec<Song> {
+    let xml_text = get_xml_text(&xml_path);
+    let playlists_xml = match roxmltree::Document::parse(&xml_text) {
+        Ok(doc) => doc,
+        Err(e) => {
+            println!("Playlist Parse Error: {}.", e);
+            process::exit(1);
+        },
+    };
+    let mut songs = Vec::new();
+    for song in playlists_xml.descendants() {
+        if song.tag_name().name() == "track" {
+            songs.push(Song{
+                title: String::from(song.descendants().
+                    find(|n| n.has_tag_name("title")).unwrap().text().unwrap()),
+                artist: String::from(song.descendants().
+                    find(|n| n.has_tag_name("creator")).unwrap().text().unwrap()),
+                album: String::from(song.descendants().
+                    find(|n| n.has_tag_name("album")).unwrap().text().unwrap())
+            });
+        }
+    }
+    songs
+}
+
+fn get_xml_text(xml_path: &str) -> String {
+    let mut file = fs::File::open(&xml_path).unwrap();
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+    text
 }
