0a0edde179e1695e127233488dcb6adf584c9608
Claire Moore <nrmoore@uci.edu>
2018-12-24 11:51:23 -0800

    Make Song Struct more powerful.
    Use std::fmt::Display trait for Song to help out, and print out song we
    have.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 extern crate roxmltree;
 
+use std::fmt;
+
 struct Song {
     title: String,
     artist: String,
     album: String,
+}
+
+impl fmt::Display for Song {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} - {} - {}", self.artist, self.title, self.album)
+    }
 }
 
 fn main() {
@@ -11,4 +19,5 @@ fn main() {
         title: String::from("風の日"),
         artist: String::from("ELLEGARDEN"),
         album: String::from("Figureheads Compilation")}];
+    print!("{}", songs[0]);
 }
